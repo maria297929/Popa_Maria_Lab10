e8e9eb93f74d18346ff0686c9ef2ca09fc86c3d9
Maria <Maria@DESKTOP-C9U8352>
2020-12-15 14:24:16 +0200

    lab11

--- Popa_Maria_Lab10/ListPage.xaml.cs
+++ Popa_Maria_Lab10/ListPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Popa_Maria_Lab10.Models;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
 
 namespace Popa_Maria_Lab10
 {
@@ -28,6 +30,23 @@ namespace Popa_Maria_Lab10
             var slist = (ShopList)BindingContext;
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
+        }
+
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
         }
     }
 }
